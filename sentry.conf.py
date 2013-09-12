8db53b34a255eff64a5e733ec50a068fce1a30f6
duy <duy@strayharbor.com>
2013-09-12 11:26:47 -0700

    Revert "Setting the timezone to Pacific time"
    This reverts commit 3aef72f14e3621a2d6f1a0f3aadb4a5b89afd78b.

--- sentry.conf.py
+++ sentry.conf.py
@@ -116,19 +116,6 @@ INSTALLED_APPS += ('django_bcrypt',)
 BCRYPT_MIGRATE = True
 
 
-# Timezone
-# --------
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Los_Angeles'
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-
 # Social Auth
 # -----------
 
